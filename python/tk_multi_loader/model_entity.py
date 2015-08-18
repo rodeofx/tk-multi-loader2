4389abff5852fd229661ffca18716d2e23969099
Adam Wierzchowski <awierzchowski@rodeofx.com>
2015-08-18 16:10:00 -0400

    Solve problem of loosing selection after items in model are reloaded

--- python/tk_multi_loader/model_entity.py
+++ python/tk_multi_loader/model_entity.py
@@ -45,22 +45,32 @@ class SgEntityModel(ShotgunModel):
         self._entity_icons["Ticket"] = QtGui.QIcon(QtGui.QPixmap(":/res/icon_Ticket_dark.png"))
         self._entity_icons["Version"] = QtGui.QIcon(QtGui.QPixmap(":/res/icon_Version_dark.png"))
         
-        ShotgunModel.__init__(self,
+        ShotgunModel.__init__(self, 
                               parent,
-                             download_thumbs=False, 
-                             schema_generation=4,
-                             bg_load_thumbs=True,
-                             bg_task_manager=bg_task_manager)
+                              download_thumbs=False, 
+                              schema_generation=4,
+                              bg_load_thumbs=True,
+                              bg_task_manager=bg_task_manager)
         fields=["image", "sg_status_list", "description"]
         self._load_data(entity_type, filters, hierarchy, fields)
-        
-        
-        
-        
+
+        self.reSelector = {'entity': None,
+                           'found_preset': None,
+                           'dialog': parent,
+                           'sel_func': None
+                           }
+        self.data_refreshed.connect(self.data_refreshed_cb)
     
     ############################################################################################
     # public methods
-    
+
+    def data_refreshed_cb(self):
+        entity = self.reSelector.get('entity', None)
+        if entity:
+            item = self.item_from_entity(entity.get('type'), entity.get('id'))
+            self.reSelector.get('sel_func')(self.reSelector.get('found_preset'), item)
+            self.reSelector['entity'] = None
+
     def async_refresh(self):
         """
         Trigger an asynchronous refresh of the model
