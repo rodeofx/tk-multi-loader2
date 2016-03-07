39d57a6015774fa06c008665d88134a7d14c887c
Jean-Christophe Morin <jcmorin@rodeofx.com>
2016-03-07 13:35:31 -0500

    python/tk-multi-loader2/dialog: remove unused stuff (RPS-243)

--- python/tk_multi_loader/dialog.py
+++ python/tk_multi_loader/dialog.py
@@ -425,17 +425,6 @@ class AppDialog(QtGui.QWidget):
         if default_action:
             default_action.trigger()
 
-    def _on_publish_filter_clicked(self):
-        """
-        Executed when someone clicks the filter button in the main UI
-        """        
-        if self.ui.search_publishes.isChecked():
-            self.ui.search_publishes.setIcon(QtGui.QIcon(QtGui.QPixmap(":/res/search_active.png")))
-            self._search_widget.enable()
-        else:
-            self.ui.search_publishes.setIcon(QtGui.QIcon(QtGui.QPixmap(":/res/search.png")))
-            self._search_widget.disable()
-
     def _on_thumbnail_mode_clicked(self):
         """
         Executed when someone clicks the thumbnail mode button
